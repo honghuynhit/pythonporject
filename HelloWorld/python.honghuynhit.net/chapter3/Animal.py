660c5926c4c43ca5422e90e6f87c082815b6cb87
honghuynhit <honghuynhit@gmail.com>
2019-05-17 09:12:10 +0700

    modify: update oop in chapter3

---
+++ HelloWorld/python.honghuynhit.net/chapter3/Animal.py
@@ -0,0 +1,15 @@
+class animal():
+    name = ''
+    age = 0
+    def __init__(self, name = '', age = 0):
+        self.name = name
+        self.age = age
+    def show(self):
+        print('My name is ', self.name, '. I am ', self.age)
+    def run(self):
+        print('Animal is running...')
+    def go(self):
+        print('Animal is going...')
+class dog(animal):
+    def run(self):
+        print(self.name,' is running...')
